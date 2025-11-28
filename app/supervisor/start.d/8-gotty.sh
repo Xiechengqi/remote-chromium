05b8bb98aa8b038d82d5afaee8586120cd22ebb0
root <root@homelab-gateway.tail71a2d0.ts.net>
2025-11-28 01:29:07 +0000

    add

--- app/supervisor/start.d/8-gotty.sh
+++ app/supervisor/start.d/8-gotty.sh
@@ -17,6 +17,7 @@ EOF
 fi
 [ ".${TERMINAL_USER}" != "." ] && [ ".${TERMINAL_PASSWORD}" != "." ] && OPTS="-c ${TERMINAL_USER}:${TERMINAL_PASSWORD}"
 echo "${TERMINAL_ONCE}" | grep -i '^true$' &> /dev/null && OPTS="${OPTS} --once"
+source ~/.profile
 INFO "/app/gotty/gotty -w -p 2222 ${OPTS} /bin/bash"
 /app/gotty/gotty -w -p 2222 ${OPTS} /bin/bash
 
