fa433dbdf244386e406083037c6d2536983d666f
Xiechengqi <xiechengqi01@gmail.com>
2025-11-30 12:06:43 +0000

    add

--- app/supervisor/start.d/15-monitor.sh
+++ app/supervisor/start.d/15-monitor.sh
@@ -3,5 +3,5 @@
 source /app/supervisor/start.d/common.sh
 
 INFO "tmux new-session -s monitor -d" && tmux new-session -s monitor -d 
-tmux send-keys -t monitor:0 'cd /app/monitor && ./check.sh' C-m
+tmux send-keys -t monitor:0 'cd /app/monitor && ./check.sh && ./reload.sh; sleep 10 && ./check.sh' C-m
 SLEEP_INFITY $0
