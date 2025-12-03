635bfcc04d8e4dcb2fd016aecbec5cc6eb3f7c2c
Xiechengqi <xiechengqi01@gmail.com>
2025-12-03 08:37:20 +0000

    add

--- app/supervisor/start.d/09-gemini-cli.sh
+++ app/supervisor/start.d/09-gemini-cli.sh
@@ -13,6 +13,13 @@ INFO "check \${NVM_DIR}/versions/node/v*/bin/npm" && ! ls ${NVM_DIR}/versions/no
 break
 done
 
+cat >> ~/.profile << EOF
+
+## Git
+alias gmt='gemini -p "根据当前项目改动生成中文commit并直接提交，commit格式:[北京时间(例如2025-12-02 13:21:22)] 中文说明" -y'
+
+EOF
+
 INFO "tmux new-session -s gemini -d" && tmux new-session -s gemini -d 
 INFO "npm -v && npm install -g @google/gemini-cli && NO_BROWSER=true gemini"
 tmux send-keys -t gemini:0 'npm -v && npm cache clean --force; npm -v && npm install -g @google/gemini-cli && NO_BROWSER=true gemini' C-m
