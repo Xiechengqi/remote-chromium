d1c0fd1e8f7db9b55713996182cb51ec1f02b634
Xiechengqi <xiechengqi01@gmail.com>
2025-12-03 21:35:00 +0800

    add

--- a.sh
+++
@@ -1,4 +0,0 @@
-cp app/index/vnc.html /app/index-chromium/
-# cp app/index/include/rfb.js /app/index-chromium/include/
-# cp app/index/js/novnc-ui.min.js /app/index-chromium/js/
-# cp app/index/js/novnc-core.min.js /app/index-chromium/js/
